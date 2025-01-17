f540fced48eb0deebb33c163a3803e50c166232d
OsherElikamel <oshercft@gmail.com>
2025-01-17 15:43:03 +0200

    QR creation with date format

---
+++ main.py
@@ -0,0 +1,17 @@
+import qrcode
+from datetime import datetime
+
+current_date = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+qr = qrcode.QRCode(
+    version=1,  
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  
+    box_size=10,  
+    border=2,  
+)
+
+qr.add_data(current_date)
+qr.make(fit=True)
+
+img = qr.make_image(fill="black", back_color="white")
+img.save("qrcode.png")

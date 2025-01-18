363a239e0e9d256ea990e40d8068baab6d4f20ae
OsherElikamel <oshercft@gmail.com>
2025-01-18 23:12:41 +0200

    refactored code

--- qr_generator.py
+++ qr_generator.py
@@ -3,22 +3,22 @@ from datetime import datetime
 import io  
 
 def generate_qr():
-    
     current_date = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
     qr = qrcode.QRCode(
-        version=1,  
-        error_correction=qrcode.constants.ERROR_CORRECT_L,  
-        box_size=10,  
-        border=2,  
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=2,
     )
-
     qr.add_data(current_date)
     qr.make(fit=True)
 
     img = qr.make_image(fill="black", back_color="white")
+    return _create_buffer(img)
 
+def _create_buffer(img):
     buffer = io.BytesIO()
-    img.save(buffer, format="PNG")  
-    buffer.seek(0)  
-    return buffer  
+    img.save(buffer, format="PNG")
+    buffer.seek(0)
+    return buffer
